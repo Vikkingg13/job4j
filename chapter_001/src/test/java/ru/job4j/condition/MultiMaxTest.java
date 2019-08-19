acaee859c04d75ee286385bc70ec95235b7bc66f
Viktor Grigoriev <14GameOver@mail.ru>
2019-08-19 15:59:43 +0300

    Программа "MultiMax" [#170815]

---
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenFirstMax(){
+        MultiMax check = new MultiMax();
+        int result = check.max(3,1,2);
+        assertThat(result,is(3));
+    }
+
+    @Test
+    public void whenSecondMax(){
+        MultiMax check = new MultiMax();
+        int result = check.max(3,4,2);
+        assertThat(result,is(4));
+    }
+
+    @Test
+    public void whenThirdMax(){
+        MultiMax check = new MultiMax();
+        int result = check.max(1,0,2);
+        assertThat(result,is(2));
+    }
+
+    @Test
+    public void whenAreEqual(){
+        MultiMax check = new MultiMax();
+        int result = check.max(3,3,3);
+        assertThat(result,is(3));
+    }
+}
