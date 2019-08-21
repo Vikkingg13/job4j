2e4c24e1624d7f942fb9174ca231271a7028022b
Viktor Grigoriev <14GameOver@mail.ru>
2019-08-21 22:50:29 +0300

    Программа "PrimeNumber" [#170828]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public int calc(int finish){
+        int result = 0;
+        for(int i = 1; i <= finish; i++){
+            for(int j = 2; j <= i; j++){
+                if(j == i) {
+                    result++;
+                }else if(i%j == 0){
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
