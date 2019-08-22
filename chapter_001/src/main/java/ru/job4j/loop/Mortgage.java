003148cf37f21d134df31f183c4fae195c3a6c7c
Viktor Grigoriev <14GameOver@mail.ru>
2019-08-22 12:26:50 +0300

    Программа "Mortgage" [#170829]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(int amount, int monthly, int percent){
+        int year = 0;
+        int sum = amount + (amount/100)*percent;
+        int sumYear = monthly * 12;
+        int paid = 0;
+        while(paid < sum){
+            paid += sumYear;
+            year++;
+        }
+        return year;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MortgageTest {
+    @Test
+    public void when1Year(){
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(1000,100,10);
+        assertThat(year, is(1));
+    }
+
+    @Test
+    public void when2Year(){
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(100,10,50);
+        assertThat(year, is(2));
+    }
+
+    @Test
+    public void when10Year(){
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(5000, 60,35);
+        assertThat(year, is(10));
+    }
+
+}
