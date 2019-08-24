09fcb828221b81c96c96198ea40a59e868aa0251
Viktor Grigoriev <14GameOver@mail.ru>
2019-08-25 00:13:17 +0300

    Программа "ArrayDuplicate" [#170838]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class ArrayDuplicate {
+
+    public String[] remove(String[] array) {
+        int finish = array.length;
+        for (int out=0; out<finish; out++) {
+            for (int in=out+1; in<finish; in++) {
+                if (array[out].equals(array[in])) {
+                    String tmp = array[array.length-1];
+                    array[array.length-1] = array[in];
+                    array[in] = tmp;
+                    finish--;
+                    in--;
+                  }
+            }
+        }
+        return  Arrays.copyOf(array,finish);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenArrayHasDuplicateThenDropIt() {
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] input = {"Olya","Olya","Nadya", "Nadya", "Masha"};
+        String[] expected = {"Olya", "Nadya", "Masha"};
+        String[] result = arrayDuplicate.remove(input);
+        assertThat(result,arrayContainingInAnyOrder(expected));
+    }
+
+    @Test
+    public void whenArrayHasNumDuplicateThenDropIt() {
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] input = {"2","1","1","1","3"};
+        String[] expected = {"1","2","3"};
+        String[] result = arrayDuplicate.remove(input);
+        assertThat(result,arrayContainingInAnyOrder(expected));
+    }
+}
