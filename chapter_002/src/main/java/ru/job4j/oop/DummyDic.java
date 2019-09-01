5f34407e4fab6eccfff6a49c4503d7a61ca03fad
Viktor Grigoriev <14GameOver@mail.ru>
2019-09-01 13:50:15 +0300

    Программа "DummyDic" [#170953]

---
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dic = new DummyDic();
+        String word = dic.engToRus("Life");
+        System.out.println(word);
+    }
+}
