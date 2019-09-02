7dba4780fd3c56ccb06157b603eb690abe89d201
Viktor Grigoriev <14GameOver@mail.ru>
2019-09-02 21:11:06 +0300

    Программа "Triangle Object" [#170944]

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -6,13 +6,12 @@ public class TriangleTest {
 
     @Test
     public void whenAreaSetThreePointsThenTriangleArea() {
-        Triangle triangle = new Triangle();
         Point first = new Point(0, 0);
         Point second = new Point(3, 0);
         Point third = new Point(2, 2);
-        double result = triangle.area(first, second, third);
+        Triangle triangle = new Triangle(first, second, third);
+        double result = triangle.area();
         double expected = 3D;
         assertThat(result, closeTo(expected, 0.1));
     }
-
 }
