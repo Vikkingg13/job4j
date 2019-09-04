6946c26731d1109971a453a20e40e0fffe742d94
Viktor Grigoriev <14GameOver@mail.ru>
2019-09-04 16:37:33 +0300

    Конструктор по умолчанию. [#173889]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Predator.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Predator extends Animal {
+
+    public Predator() {
+        super();
+        System.out.println("Class name: Predator");
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Tiger.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Tiger extends Predator {
+
+    public Tiger() {
+        super();
+        System.out.println("Class name: Tiger");
+    }
+
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
