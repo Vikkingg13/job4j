aab1b256892f53b36b0d73285c0e6186cc376978
Viktor Grigoriev <14GameOver@mail.ru>
2019-09-09 12:02:14 +0300

    Annotations Before, After [#170926]

--- chapter_002/src/test/java/ru/job4j/oop/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/oop/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.oop;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -7,14 +9,27 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         String ln = System.getProperty("line.separator");
-        assertThat(new String(out.toByteArray()),
+        assertThat(this.out.toString(),
                 is(new StringBuilder()
                         .append("+++++++" + ln)
                         .append("+++++++" + ln)
@@ -23,17 +38,13 @@ public class PaintTest {
                         .toString()
                 )
         );
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         String ln = System.getProperty("line.separator");
-        assertThat(new String(out.toByteArray()),
+        assertThat(this.out.toString(),
                 is(new StringBuilder()
                         .append("   +   " + ln)
                         .append("  +++  " + ln)
@@ -42,6 +53,5 @@ public class PaintTest {
                         .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
