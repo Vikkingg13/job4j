61a8b802290603b0e57cc06a4b95d834b5d77484
Viktor Grigoriev <14GameOver@mail.ru>
2019-09-11 20:41:27 +0300

    Шаблон Декоратор [#170934]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -34,6 +34,6 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        new StartUI(new ValidateInput(), new Tracker()).init();
+        new StartUI(new ValidateInput(new ConsoleInput()), new Tracker()).init();
     }
 }
