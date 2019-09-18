5f7e4d18daee42b9f295acdc6feecc1648a18a7e
Viktor Grigoriev <14GameOver@mail.ru>
2019-09-18 21:28:55 +0300

    Телефонный справочник на базе ArrayList[#170899]

---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary dictionary = new PhoneDictionary();
+        dictionary.add(new Person("Alexander","Lukashenko", "3722176", "Minsk"));
+        dictionary.add(new Person("Alexey", "Navalny", "651484", "Moscow"));
+        List<Person> persons = dictionary.find("Moscow");
+        assertThat(persons.iterator().next().getSurname(), is("Navalny"));
+    }
+}
