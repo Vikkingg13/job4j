1d0bef3a9fdccd2ff1f7b02215eb7cfdd3684243
Viktor Grigoriev <14GameOver@mail.ru>
2019-10-16 23:10:54 +0300

    Итератор для двухмерного массива [#189412]

---
+++ chapter_005/src/main/java/ru/job4j/collections/IteratorArray.java
@@ -0,0 +1,28 @@
+package ru.job4j.collections;
+
+import java.util.Iterator;
+
+public class IteratorArray implements Iterator<Integer> {
+
+    private final int[][] array;
+    private int rows = 0;
+    private int cells = 0;
+
+    public IteratorArray(final int[][] array) {
+        this.array = array;
+    }
+
+    public boolean hasNext() {
+        return array.length > rows && array[rows].length > cells;
+    }
+
+    public Integer next() {
+        int result = array[rows][cells];
+        cells++;
+        if (array[rows].length == cells) {
+            cells = 0;
+            rows ++;
+        }
+        return result;
+    }
+}
