03bf1f1ee42cb674fb5ef8a896d7fd04b7b76202
Vikkingg13 <14GameOver@mail.ru>
2021-08-29 12:27:28 +0300

    Reference types [#6854]

---
+++ chapter_009/src/main/java/ru/job4j/reference/SoftDemo.java
@@ -0,0 +1,44 @@
+package ru.job4j.reference;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SoftDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        example1();
+//        example2();
+    }
+
+    public static void example1() throws InterruptedException {
+        Object strong = new Object();
+        SoftReference<Object> soft = new SoftReference<>(strong);
+        strong = null;
+        System.out.println(soft.get());
+    }
+
+    public static void example2() {
+        List<SoftReference<Object>> objects = new ArrayList<>();
+        for (int i = 0; i < 100_000_000; i++) {
+            objects.add(new SoftReference<>(new Object() {
+                String value = String.valueOf(System.currentTimeMillis());
+
+                @Override
+                protected void finalize() throws Throwable {
+                    System.out.println("Object removed!");
+                }
+            }));
+        }
+        System.gc();
+        int liveObject = 0;
+        for (SoftReference<Object> ref : objects) {
+            Object object = ref.get();
+            if (object != null) {
+                liveObject++;
+            }
+        }
+        System.out.println(liveObject);
+    }
+}
+
---
+++ chapter_009/src/main/java/ru/job4j/reference/StrongDemo.java
@@ -0,0 +1,50 @@
+package ru.job4j.reference;
+
+import java.util.concurrent.TimeUnit;
+
+public class StrongDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+//        example1();
+        example2();
+    }
+
+    public static void example1() throws InterruptedException {
+        Object[] objects = new Object[100];
+        for (int i = 0; i < 100; i++) {
+            objects[i] = new Object() {
+                @Override
+                protected void finalize() throws Throwable {
+                    super.finalize();
+                    System.out.println("Object removed");
+                }
+            };
+        }
+        for (int i = 0; i < 100; i++) {
+            objects[i] = null;
+        }
+        System.gc();
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    public static void example2() throws InterruptedException {
+        Object[] objects = new Object[100];
+        for (int i = 0; i < 100; i++) {
+            Object object = new Object() {
+                Object innerObject = new Object() {
+                    @Override
+                    protected void finalize() throws Throwable {
+                        super.finalize();
+                        System.out.println("Inner Object removed");
+                    }
+                };
+            };
+            objects[i] = object;
+        }
+        for (int i = 0; i < 100; i++) {
+            objects[i] = null;
+        }
+        System.gc();
+        TimeUnit.SECONDS.sleep(5);
+    }
+}
